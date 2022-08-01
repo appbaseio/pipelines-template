b8f67435c727803a504a4f90039c17a99e5e83fe
Deepjyoti Barman <deep.barman30@gmail.com>
2022-08-01 13:37:20 +0530

    Make the vector field where vector is indexed into dynamic

--- vector_indexing/mergeVector.js
+++ vector_indexing/mergeVector.js
@@ -1,13 +1,14 @@
 function handleRequest() {
     const requestBody = JSON.parse(context.request.body)
     const lyricVector = JSON.parse(context["lyric_vector"])
+    const vectorFieldName = context.envs.VECTOR_FIELD_NAME
 
     return {
         request: {
             ...context.request,
             body: JSON.stringify({
                 ...requestBody,
-                "lyric_vector": lyricVector.data[0].embedding
+                vectorFieldName: lyricVector.data[0].embedding
             })
         }
     }
