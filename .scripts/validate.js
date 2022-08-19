1f8ed9145d3399bb98b5e2cd7d0a50ed428e6698
Deepjyoti Barman <deep.barman30@gmail.com>
2022-08-19 20:48:29 +0530

    Add some code to parse the pipeline file

--- .scripts/validate.js
+++ .scripts/validate.js
@@ -7,6 +7,7 @@
  * should be the validation details for that particular pipeline.
  */
 import fetch from "node-fetch";
+const fs = require('fs')
 
 import { config } from "./config.js";
 
@@ -17,6 +18,8 @@ async function checkUpstreamActive(appbaseURL) {
      * 
      * @param {string} appbaseURL APPBASE_URL to verify whether or not
      * it is active.
+     * 
+     * @returns {Boolean} Indicates whether or not upstream is active
      */
     URLtoHit = `${appbaseURL}/arc/_health`
 
@@ -24,6 +27,38 @@ async function checkUpstreamActive(appbaseURL) {
 
     return response.statusCode == 200;
 }
+
+
+function parsePipelineFile(pipelinePath) {
+    /**
+     * Parse the pipeline file based on the passed path.
+     * 
+     * This function will also check whether or not the file
+     * is present and accordingly throw errors.
+     * 
+     * Assumption is that the file will be a JSON file since
+     * the upstream endpoint expects JSON content as well.
+     * 
+     * @param {string} pipelinePath Path to the pipeline file
+     * 
+     * @returns {string} Contents of the pipeline file
+     */
+
+    // Check if the extension is JSON
+    if (pipelinePath.toLower().split(".").pop() != "json") {
+        throw Error("invalid pipeline path passed: file is non JSON")
+    }
+
+    if (!fs.existsSync(pipelinePath)) {
+        throw Error("invalid pipeline path passed: does not exist!")
+    }
+
+    // Parse the file since it exists.
+    let fileContent = fs.readFileSync(pipelinePath);
+    let parsedJSONContent = JSON.parse(fileContent);
+    return JSON.stringify(parsedJSONContent);
+}
+
 
 async function validatePipeline() {
     /**
@@ -52,7 +87,22 @@ async function validatePipeline() {
 
     console.log("Upstream is active: status received is OK!")
 
-    // TODO: Parse the pipeline one-click JSON file to get it ready for sending the request.
+    // Parse the pipeline one-click JSON file to get it ready for sending the request.
+    var pipelineContent = null
+    try {
+        pipelineContent = parsePipelineFile(pathToPipeline);
+    } catch (err) {
+        console.error("error while parsing pipeline file: ", err)
+        return
+    }
 
+    if (pipelineContent == null) {
+        console.error("no content present in the passed pipeline file!")
+        return
+    }
+
+    console.log("pipeline file parsed successfully!")
+
+    // TODO: Parse the validator file
 
 }
