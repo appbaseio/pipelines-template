19b0eac2902fb723ea3e7c369b8ce39a04dcbd9f
Deepjyoti Barman <deep.barman30@gmail.com>
2022-08-19 20:23:12 +0530

    Add code to verify the upstream APPBASE_URL passed

--- .scripts/validate.js
+++ .scripts/validate.js
@@ -6,8 +6,26 @@
  * this script as the first argument and the second argument
  * should be the validation details for that particular pipeline.
  */
+import fetch from "node-fetch";
 
-function validatePipeline() {
+import { config } from "./config.js";
+
+async function checkUpstreamActive(appbaseURL) {
+    /**
+     * Check whether or not the APPBASE_URL passed
+     * is active by hitting the `/arc/_health` endpoint.
+     * 
+     * @param {string} appbaseURL APPBASE_URL to verify whether or not
+     * it is active.
+     */
+    URLtoHit = `${appbaseURL}/arc/_health`
+
+    response = await fetch(URLtoHit)
+
+    return response.statusCode == 200;
+}
+
+async function validatePipeline() {
     /**
      * Validate the pipeline and take care of everything
      * else linked to that.
@@ -22,7 +40,19 @@ function validatePipeline() {
     pathToPipeline = argsPassed[0];
     pathToValidateFile = argsPassed[1];
 
+    // Parse the configuration to extract the APPBASE_URL
+    appbaseURL = config.APPBASE_URL
+
+    // Verify that upstream is active
+    isUpstreamActive = await checkUpstreamActive(appbaseURL)
+    if (!isUpstreamActive) {
+        console.error("upstream is not active, `/arc/_health` returned a non OK status code!")
+        return
+    }
+
+    console.log("Upstream is active: status received is OK!")
+
     // TODO: Parse the pipeline one-click JSON file to get it ready for sending the request.
 
-    // TODO: Parse the configuration to extract the APPBASE_URL
+
 }
